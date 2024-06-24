b64f8d1adfc5616b004501d5b0f9f6ee6d740aa4
Chris Annunziato <NeonMeteors@gmail.com>
2024-06-23 23:24:39 -0400

    issue with build

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 600
+	screenHeight = 800
+)
 
 func main() {
-	sdl.Init(sdl.INIT_EVERYTHING)
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL:", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"gaming in go ep. 2",
+		sdl.WINDOWPOS_CENTERED,
+		sdl.WINDOWPOS_CENTERED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("Creating window", err)
+		return
+	}
+
+	defer window.Destroy()
+
+	sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("Creating renderer", err)
+		return
+	}
 
 }
